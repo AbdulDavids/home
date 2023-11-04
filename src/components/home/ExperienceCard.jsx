f9667f3af3562f8a77de7f9a2bca2ff6aae2316c
AbdulDavids <abdulbaaridavids04@gmail.com>
2023-11-04 20:11:21 +0200

    fix

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -8,7 +8,21 @@ const ExperienceCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+      <img
+  className="bg-white mb-3"
+  src={data.companylogo}
+  alt=""
+  width="100"
+  height="100"
+  style={{
+    borderRadius: '50%', // Makes the image circular
+    objectFit: 'cover', // Maintains the aspect ratio of the image
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)', // Adds a drop shadow below the image
+  }}
+/>
+
+
+
         <p className="lead">
           {data.role}
           <br />
--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,31 +6,27 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
-  middleName: "",
-  lastName: "Shoaib",
+  firstName: "Abdul",
+  middleName: "Baari",
+  lastName: "Davids",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      url: "https://github.com/abduldavids",
     },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
+    //{
+    //  image: "fa-instagram",
+    //  url: "https://www.instagram.com/username/",
+    //},
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
+      url: "https://www.linkedin.com/in/abduldavids/",
     },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
+    //{
+    //  image: "fa-twitter",
+    //  url: "https://www.twitter.com/hashirshoaeb/",
+    //},
   ],
 };
 
@@ -51,11 +47,11 @@ const mainBody = {
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/abduldavids.jpeg"),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    "My name is Abdul Baari. I’m a software engineer currently studying towards my BSc in Computer Science. I'm passionate about new technologies, especially artificial intelligence. I'm looking for opportunities to work on projects that will help me grow as a developer and contribute to society.",
+  resume: "https://docs.google.com/document/d/1yi6HG69-CCjXuHrV0Z_pAkan2BQBMHIL/edit?usp=sharing&ouid=112174512699894154976&rtpof=true&sd=true",
 };
 
 // PROJECTS SECTION
@@ -66,9 +62,9 @@ const about = {
 const repos = {
   show: true,
   heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  gitHubUsername: "abduldavids", //i.e."johnDoe12Gh"
   reposLength: 4,
-  specificRepos: [],
+  specificRepos: ["bulletin", "lifetimer"],
 };
 
 // Leadership SECTION
@@ -100,24 +96,24 @@ const skills = {
   show: true,
   heading: "Skills",
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
+    { name: "Python", value: 80 },
     { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: "Java", value: 75 },
+    { name: "SQL", value: 55 },
+    { name: "Java", value: 90 },
+    { name: "PHP", value: 65 },
+    { name: "Git", value: 80 },
+    { name: "JavaScript", value: 70 },
   ],
   softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
     { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
+    { name: "Adaptability", value: 85 },
     { name: "Creativity", value: 90 },
+    { name: "Detail-Oriented", value: 80 },
+    { name: "Collaboration", value: 90 },
+    { name: "Initiative", value: 75 },
+    { name: "Learning Skills", value: 70 },
+
   ],
 };
 
@@ -126,23 +122,23 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+    "I'm currently looking for part-time Software Engineering opportunities to pursue alongside my studies! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "abdulbaaridavids04@gmail.com",
 };
 
 const experiences = {
-  show: false,
+  show: true,
   heading: "Experiences",
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      role: 'Junior Software Engineer at CodeInfinity',// Here Add Company Name
+      companylogo: require('../assets/img/codeinfinitylogo.jpg'),
+      date: 'October 2023 – Present',
     },
     {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
+      role: 'Web Developer',
+      companylogo: require('../assets/img/finishline.jpg'),
+      date: 'March 2022 – October 2022',
     },
   ]
 }
